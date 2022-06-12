d29c4b282a93debcaabeb1f9acff39a67d095111
Vinay Chauhan <vinaychauhan.22@outlook.com>
2022-06-12 12:50:34 +0530

    Revert "add git chnages"
    This reverts commit 4927fc9f10d2784b53b9ca9f3a40873ae546766d.

---
+++ WiredBrainCofee/WiredBrainCofee/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WiredBrainCofee.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ WiredBrainCofee/WiredBrainCofee/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WiredBrainCofee.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
