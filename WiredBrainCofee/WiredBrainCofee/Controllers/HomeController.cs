a380dde43255de491e8fc2ca1322ab124eb16b81
Vinay <vc063@gmail.com>
2022-06-12 12:04:06 +0530

    Add Wired Brain Coffee Project

---
+++ WiredBrainCofee/WiredBrainCofee/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WiredBrainCofee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Wired Brain Coffee Home Page";
+
+            return View();
+        }
+    }
+}
